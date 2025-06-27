8326ee486097946601560013466687b101d18759
WilsonBright7 <wilbrightson@gmail.com>
2025-06-27 16:45:13 +0100

    final

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,62 +1,66 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
-import { Platform } from 'react-native';
-
-
-import { IconSymbol } from '@/components/ui/IconSymbol';
-import TabBarBackground from '@/components/ui/TabBarBackground';
-import  COLORS from '@/constants/Colors';
+import { Tabs } from "expo-router";
+import { Platform } from "react-native";
+import COLORS from "@/constants/Colors";
+import { MaterialIcons } from "@expo/vector-icons";
 
 export default function TabLayout() {
-
-
   return (
     <Tabs
       screenOptions={{
         tabBarActiveTintColor: COLORS.primary,
         headerShown: false,
-
-        tabBarBackground: TabBarBackground,
-        tabBarStyle: Platform.select({
-          ios: {
-            // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
-          },
-          default: {},
-        }),
-      }}>
+        tabBarStyle: {
+          backgroundColor: COLORS.background,
+          ...Platform.select({
+            ios: {
+              position: "absolute",
+            },
+            default: {},
+          }),
+        },
+      }}
+    >
       <Tabs.Screen
         name="home"
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="create"
-        options={{
-          title: 'Create',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          title: "Home",
+          tabBarIcon: ({ color, size }) => (
+            <MaterialIcons name="home" color={color} size={size} />
+          ),
         }}
       />
 
       <Tabs.Screen
         name="profile"
         options={{
-          title: 'Profile',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          title: "Profile",
+          tabBarIcon: ({ color, size }) => (
+            <MaterialIcons name="person" color={color} size={size} />
+          ),
         }}
       />
-
+      <Tabs.Screen
+        name="create"
+        options={{
+          title: "Create",
+          tabBarIcon: ({ color, size }) => (
+            <MaterialIcons
+              name="add-circle-outline"
+              color={color}
+              size={size}
+            />
+          ),
+        }}
+      />
       <Tabs.Screen
         name="saved"
         options={{
-          title: 'Saved',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          title: "Saved",
+          tabBarIcon: ({ color, size }) => (
+            <MaterialIcons name="bookmark" color={color} size={size} />
+          ),
         }}
       />
     </Tabs>
-
-    
   );
 }
